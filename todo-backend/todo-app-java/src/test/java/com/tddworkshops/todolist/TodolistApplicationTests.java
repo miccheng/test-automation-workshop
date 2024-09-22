5f4027c07610d30f0b0a432bb47e42cb847abcdb
Michael Cheng <michael_cheng@tech.gov.sg>
2024-09-22 17:54:40 +0800

    Add Integration test using WebTestClient
    Co-authored-by: Anarane Tung <ana@anarane.com>

--- todo-backend/todo-app-java/src/test/java/com/tddworkshops/todolist/TodolistApplicationTests.java
+++ todo-backend/todo-app-java/src/test/java/com/tddworkshops/todolist/TodolistApplicationTests.java
@@ -1,13 +1,103 @@
 package com.tddworkshops.todolist;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@SpringBootTest
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class TodolistApplicationTests {
+	@Autowired
+	WebTestClient webTestClient;
 
 	@Test
-	void contextLoads() {
+	@Order(1)
+	void context_loads() {
 	}
 
+	@Test
+	@Order(2)
+	void context_with_web_test_client() {
+		assertNotNull(webTestClient);
+		webTestClient.get()
+				.uri("/todos")
+				.exchange()
+				.expectStatus()
+				.isOk();
+	}
+
+	@Test
+	@Order(3)
+	void test_create_todo() {
+		webTestClient.post()
+				.uri("/todos")
+				.contentType(MediaType.APPLICATION_JSON)
+				.bodyValue("{\"task\": \"New Task\"}")
+				.exchange()
+				.expectStatus()
+				.isCreated();
+
+		webTestClient.get()
+				.uri("/todos")
+				.exchange()
+				.expectStatus()
+				.isOk()
+				.expectBody()
+				.jsonPath("_embedded.todos.length()")
+				.isEqualTo("1")
+				.jsonPath("_embedded.todos[0].task")
+				.isEqualTo("New Task");
+	}
+
+	// Test the edit Todo feature
+	@Test
+	@Order(4)
+	void test_edit_todo() {
+		webTestClient.put()
+				.uri("/todos/1")
+				.contentType(MediaType.APPLICATION_JSON)
+				.bodyValue("{\"task\": \"Edited Task\", \"completed\": true}")
+				.exchange()
+				.expectStatus()
+				.isCreated();
+
+		// Validate the updated value
+		webTestClient.get()
+				.uri("/todos/1")
+				.exchange()
+				.expectStatus()
+				.isOk()
+				.expectBody()
+				.jsonPath("task")
+				.isEqualTo("Edited Task")
+				.jsonPath("completed")
+				.isEqualTo("true");
+	}
+
+	// Test the delete Todo feature
+	@Test
+	@Order(5)
+	void test_delete_todo() {
+		webTestClient.delete()
+				.uri("/todos/1")
+				.exchange()
+				.expectStatus()
+				.isOk();
+
+		// Validate the deleted value
+		webTestClient.get()
+				.uri("/todos/1")
+				.exchange()
+				.expectStatus()
+				.isNotFound();
+	}
 }
