f9e73e555e5de21ce18773898366901cd40b42cf
Michael Cheng <michael_cheng@tech.gov.sg>
2024-09-22 20:42:26 +0800

    Tweaks to the Java environment
    - Add Vue extension in the Devcontainer to help debug Vue3 front-end app.
    - NPM install dependencies in the Vue3 app.
    - Ensure that Spring Data Rest supports CORS.
    - Update the return payload of Create and Update endpoints.

--- todo-backend/todo-app-java/src/main/java/com/tddworkshops/todolist/repository/TodoRepository.java
+++ todo-backend/todo-app-java/src/main/java/com/tddworkshops/todolist/repository/TodoRepository.java
@@ -2,9 +2,11 @@ package com.tddworkshops.todolist.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.tddworkshops.todolist.entity.Todo;
 
 @RepositoryRestResource
+@CrossOrigin
 public interface TodoRepository extends CrudRepository<Todo, Long> {
 }
