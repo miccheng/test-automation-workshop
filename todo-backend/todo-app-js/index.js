c257cd1a2f6cf3c556a20cb52c9361e82afef1ff
Michael Cheng <mcheng.work@gmail.com>
2024-10-17 04:39:46 +0000

    feat(AC-1): Implement backend for clearing completed tasks.

--- todo-backend/todo-app-js/index.js
+++ todo-backend/todo-app-js/index.js
@@ -73,6 +73,21 @@ app.delete('/todos/:id', async (req, res) => {
     }
 });
 
+// Delete tasks that has been marked completed
+app.post('/todos/clear-completed', async (req, res) => {
+    try {
+        await ToDo.destroy({
+            where: {
+                completed: true
+            }
+        });
+
+        res.json({ message: 'Completed Task Deleted' });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
 // Start the server
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
