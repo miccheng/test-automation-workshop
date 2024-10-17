d9da08c40adb40a52867205e07a7dd797e41de61
Michael Cheng <mcheng.work@gmail.com>
2024-10-17 08:13:12 +0000

    feat(AC-4): Add e2e test coverage

--- e2e/js/tests/todo-app.spec.js
+++ e2e/js/tests/todo-app.spec.js
@@ -107,3 +107,36 @@ test('add multiple todo items', async ({ page }) => {
   locator = page.locator('.todo-item:nth-child(2)');
   await expect(locator).toHaveText(/Buy eggs/);
 });
+
+test('clear completed To Do items', async ({ page }) => {
+  await page.goto(testHost);
+
+  // Add a todo item
+  await addItem(page, 'Buy milk');
+
+  // Verify the item is added
+  const firstItem = page.locator('.todo-item:nth-child(1)');
+  await expect(firstItem).toHaveText(/Buy milk/);
+
+  // Add another todo item
+  await addItem(page, 'Buy eggs');
+
+  // Verify the item is added
+  const secondItem = page.locator('.todo-item:nth-child(2)');
+  await expect(secondItem).toHaveText(/Buy eggs/);
+
+  // Clicks the done checkbox for the 2nd item
+  await secondItem.getByRole('checkbox').click();
+  const secondItemText = secondItem.locator('span');
+  await expect(secondItemText).toHaveClass('completed');
+
+  let clearBtn = page.locator('.clearCompletedBtn'); 
+  await expect(clearBtn).toBeVisible();
+
+  // Clicks the Clear Completed button
+  await clearBtn.click();
+
+  // Ensure that the 2nd item has been cleared
+  await expect(secondItem).not.toBeVisible();
+  await expect(firstItem).toBeVisible();
+});
