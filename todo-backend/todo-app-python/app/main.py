96191bcb26e6e8bc86bb92c019998d1689a3ace5
Michael Cheng <mcheng.work@gmail.com>
2024-10-18 14:02:18 +0800

    Fix issue with path.

--- todo-backend/todo-app-python/app/main.py
+++ todo-backend/todo-app-python/app/main.py
@@ -18,12 +18,12 @@ app.add_middleware(
 )
 
 
-@app.post("/todos/", response_model=schemas.Todo)
+@app.post("/todos", response_model=schemas.Todo)
 def create_todo(todo: schemas.TodoCreate, db: Session = Depends(get_db)):
     return crud.create_todo(db=db, todo=todo)
 
 
-@app.get("/todos/", response_model=list[schemas.Todo])
+@app.get("/todos", response_model=list[schemas.Todo])
 def read_todos(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     todos = crud.get_todos(db, skip=skip, limit=limit)
     return todos
