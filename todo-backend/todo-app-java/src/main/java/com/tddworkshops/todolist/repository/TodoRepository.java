cd553d8817a1ccf636bd2069cf4b6014e1bae7f8
anarane.tung <anarane.tung@fdmgroup.com>
2024-10-25 12:32:58 +0000

    feat(AC-1): Implement java backend for clearing completed tasks.

--- todo-backend/todo-app-java/src/main/java/com/tddworkshops/todolist/repository/TodoRepository.java
+++ todo-backend/todo-app-java/src/main/java/com/tddworkshops/todolist/repository/TodoRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TodoRepository extends JpaRepository<Todo, Long> {
+    void deleteByCompleted(boolean completed);
 }
