98de76ab0b7184190e09fc34e4afb2d53c0e557b
Michael Cheng <mcheng.work@gmail.com>
2024-10-30 00:44:08 +0000

    Add test coverage.

--- todo-backend/todo-app-java/src/test/java/com/tddworkshops/todolist/repository/TodoRepositoryTest.java
+++ todo-backend/todo-app-java/src/test/java/com/tddworkshops/todolist/repository/TodoRepositoryTest.java
@@ -30,4 +30,28 @@ public class TodoRepositoryTest {
         assertThat(todoList.get(0).getTask()).isEqualTo("Task 1");
         assertThat(todoList.get(1).getTask()).isEqualTo("Task 2");
     }
+
+    @Test
+    void whenDeletedByCompleted_thenReturnAllUnclearedTodos() {
+        // Arrange
+        Todo task1 = new Todo(null, "Incomplete Task", false);
+        Todo task2 = new Todo(null, "Completed Task", true);
+        todoRepository.save(task1);
+        todoRepository.save(task2);
+
+        // Act - Remove all completed tasks
+        todoRepository.deleteByCompleted(true);
+        
+        // Assert
+        List<Todo> todoList = todoRepository.findAll();
+        assertThat(todoList).hasSize(1);
+        assertThat(todoList.get(0).getTask()).isEqualTo("Incomplete Task");
+
+        // Act - Remove all incomplete tasks
+        todoRepository.deleteByCompleted(false);
+
+        // Assert
+        todoList = todoRepository.findAll();
+        assertThat(todoList).hasSize(0);
+    }
 }
